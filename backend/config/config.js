bff5c91cbe3e61a4bd59983122143310ccc7b72c
Joaquin Esteban Lobos Parra <jotawolf@pop-os.localdomain>
2020-09-22 19:35:10 -0300

    :tada: Esqueleto Backend

---
+++ backend/config/config.js
@@ -0,0 +1,14 @@
+// module.exports={
+//   username:'postgres',  
+//   password:'586vm5jm',
+//   database: 'muchos-a-muchos', 
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   seederStorage:'json',
+//   seederStoragePath:'sequelizeSeeds.json',
+//   migrationStorage: "json",
+//   migrationStoragePath: "sequelizeMigrate.json",
+//   define:{
+//     timestamps:false
+//   }
+// }
---
+++ backend/src/database/db.js
@@ -0,0 +1,2 @@
+import {Sequelize,DataTypes} from 'sequelize'
+import config  from '../../config/config'
