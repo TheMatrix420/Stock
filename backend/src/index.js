71b11314ff79f2a70e58f29e1394fd83fc6b9b3f
josebernal321 <61957579+josebernal321@users.noreply.github.com>
2020-09-22 20:33:14 -0300

    :wrench: Configuracion inicial de express y sequelize-cli

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express,{json} from 'express'
+import morgan from 'morgan'
+import router from './routers/index'
+import sequelize from  './database/db'
+
+const app=express()
+
+app.use(morgan('dev'));
+app.use(json())
+app.use(express.urlencoded({ extended: false }))
+// app.use(router)
+
+
+app.listen(4000,()=>{
+    console.log('Se esta escuchando el puerto 4000');
+    sequelize.authenticate().then(()=>{
+        console.log('se establecion la conexion con la bd')
+    })
+})
