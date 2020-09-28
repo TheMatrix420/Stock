0ae978716d43278b170c8dee269739623424acc5
Jota-Wolf <lobos.joaquin@hotmail.com>
2020-09-27 23:53:51 -0300

    :hammer: Arreglo config db

--- config/config.js
+++ config/config.js
@@ -3,19 +3,40 @@ process.env.PORT = process.env.PORT || 4000;
 //Token
 process.env.CADUCIDAD_TOKEN = "48h";
 //SEED DE AUTENTICACION
-process.env.SEED = process.env.SEED || "eyJzYWRhZGFzZCI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9";
+process.env.SEED =
+  process.env.SEED ||
+  "eyJzYWRhZGFzZCI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9";
+//ENTORNO
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
-module.exports = {
-  username: "postgres",
-  password: "fenix5769",
-  database: "stock",
-  host: "localhost",
-  dialect: "postgres",
-  seederStorage: "json",
-  seederStoragePath: "sequelizeSeeds.json",
-  migrationStorage: "json",
-  migrationStoragePath: "sequelizeMigrate.json",
-  define: {
-    timestamps: false,
-  },
-};
+if (process.env.NODE_ENV === "dev") {
+  module.exports = {
+    username: "postgres",
+    password: "fenix5769",
+    database: "stock",
+    host: "localhost",
+    dialect: "postgres",
+    seederStorage: "json",
+    seederStoragePath: "sequelizeSeeds.json",
+    migrationStorage: "json",
+    migrationStoragePath: "sequelizeMigrate.json",
+    define: {
+      timestamps: false,
+    },
+  };
+} else {
+  module.exports = {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    seederStorage: "json",
+    seederStoragePath: "sequelizeSeeds.json",
+    migrationStorage: "json",
+    migrationStoragePath: "sequelizeMigrate.json",
+    define: {
+      timestamps: false,
+    },
+  };
+}
